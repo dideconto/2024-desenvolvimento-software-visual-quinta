ca4e0d8c9de3ad59ad84fcd4c5acfb40645a744c
Auto Logon <autologon@CSED.LAB>
2024-08-29 11:32:29 -0300

    API - Exemplos de requisições via GET

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,6 +1,30 @@
+//Testar as APIs
+// - Rest Client - Extensão do VS Code
+// - Postman
+// - Insomnia
+//Minimal APIs
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Endpoints - Funcionalidade
+//Request/Requisição - URL e o método/verbo HTTP
+app.MapGet("/", () => "Hello World em C#!");
+
+app.MapGet("/segundafuncionalidade", () => "Segunda funcionalidade");
+
+app.MapGet("/retornarendereco", () =>
+{
+    dynamic endereco = new
+    {
+        rua = "Praça Osório",
+        numero = 125
+    };
+    return endereco;
+});
+
+//Criar novas funcionalidades/Endpoints para receber dados
+// - Pelo URL da requisição
+// - Corpo da requisição
+//Guardar as informações em uma lista
 
 app.Run();
