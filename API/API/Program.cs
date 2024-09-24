17d08fea0f5911938dd247f33e651e34a3d4f7f9
Diogo Steinke Deconto <dideconto@gmail.com>
2024-09-24 15:50:06 -0300

    API - CRUD completo de Produtos

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -28,10 +28,10 @@ List<Produto> produtos =
 //Endpoints - Funcionalidade
 //Request/Requisição - URL e o método/verbo HTTP
 //Response/Resposta - Dados (json ou xml) e códigos de status HTTP
-app.MapGet("/", () => "API de Produtos!");
+app.MapGet("/", () => "API de Produtos");
 
-//GET: http://localhost:5117/produto/listar
-app.MapGet("/produto/listar", () =>
+//GET: /api/produto/listar
+app.MapGet("/api/produto/listar", () =>
 {
     if (produtos.Count > 0)
     {
@@ -40,28 +40,48 @@ app.MapGet("/produto/listar", () =>
     return Results.NotFound();
 });
 
-//GET: http://localhost:5117/produto/buscar/agua
-app.MapGet("/produto/buscar/{nome}", ([FromRoute] string nome) =>
+//GET: /api/produto/buscar/{id}
+app.MapGet("/api/produto/buscar/{id}", ([FromRoute] string id) =>
 {
-    foreach (Produto produtoCadastrado in produtos)
+    Produto? produto = produtos.Find(x => x.Id == id);
+    if (produto == null)
     {
-        if (produtoCadastrado.Nome == nome)
-        {
-            return Results.Ok(produtoCadastrado);
-        }
+        return Results.NotFound();
     }
-    return Results.NotFound();
+    return Results.Ok(produto);
 });
 
-//POST: http://localhost:5117/produto/cadastrar
-app.MapPost("/produto/cadastrar", ([FromBody] Produto produto) =>
+//POST: /api/produto/cadastrar/param_nome
+app.MapPost("/api/produto/cadastrar", ([FromBody] Produto produto) =>
 {
     produtos.Add(produto);
     return Results.Created("", produto);
 });
 
-//Implementar todas as funcionalidades do CRUD
-// - Remover produto
-// - Alterar produto
+//DELETE: /api/produto/deletar/{id}
+app.MapDelete("/api/produto/deletar/{id}", ([FromRoute] string id) =>
+{
+    Produto? produto = produtos.Find(x => x.Id == id);
+    if (produto == null)
+    {
+        return Results.NotFound();
+    }
+    produtos.Remove(produto);
+    return Results.Ok(produto);
+});
+
+//PUT: /api/produto/alterar/{id}
+app.MapPut("/api/produto/alterar/{id}", ([FromRoute] string id, [FromBody] Produto produtoAlterado) =>
+{
+    Produto? produto = produtos.Find(x => x.Id == id);
+    if (produto == null)
+    {
+        return Results.NotFound();
+    }
+    produto.Nome = produtoAlterado.Nome;
+    produto.Quantidade = produtoAlterado.Quantidade;
+    produto.Preco = produtoAlterado.Preco;
+    return Results.Ok(produto);
+});
 
 app.Run();
